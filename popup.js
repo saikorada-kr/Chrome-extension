47cb0f23440126c4a4febf1b924880e6cd864a5d
saikorada-kr <saikorada.kr>
2019-11-10 17:29:56 +0530

    giving saved text over alert

--- popup.js
+++ popup.js
@@ -2,4 +2,14 @@ $(function(){
     $('name').keyup(function(){
         $('#text').text(('#text').val());
     });
+        $("#btn1").click(function(){
+        var value=$('textarea#text').val();
+        chrome.storage.sync.set ({'mynotes': value});
+});
+    $("#btn").click(function(){
+        chrome.storage.sync.get('mynotes',function(data){
+         alert(data.mynotes);
+         console.log(mynotes)
+        });
+    }); 
 });
